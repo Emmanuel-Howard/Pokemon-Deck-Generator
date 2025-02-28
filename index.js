05442db103849f929ad60641b49fa775fc72ddb0
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-28 13:43:56 -0500

    Defining randomImage choice

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Need to: onclick. + generate card movement
+// Setting constants & Variables
+
+const generateBtn = document.getElementById("generateBtn");
+const topCard = document.getElementById("topCard");
+const emptyCard1 = document.getElementById("emptyCard1");
+const emptyCard2 = document.getElementById("emptyCard2");
+const emptyCard3 = document.getElementById("emptyCard3");
+const emptyCard4 = document.getElementById("emptyCard4");
+let cardList = ["Pokemon Cards/bulbasaur.webp", "Pokemon Cards/charizard.png", "Pokemon Cards/charmander.png", "Pokemon Cards/eevee.png", "Pokemon Cards/pikachu VMAX.png", "Pokemon Cards/pikachu.png", "Pokemon Cards/snorlax.png", "Pokemon Cards/squirtle.png"];
+let newCardList = []
+let randomImage = cardList[Math.floor(Math.random() * cardList.length)];
+console.log(randomImage)
+
+// On click function
+generateBtn.onclick = function generate(){
+
+};
+
