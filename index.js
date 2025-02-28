21f7317a9872b5643a4eb571d735187c51bbf794
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-28 13:56:00 -0500

    First version of changeImage based on random number

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const emptyCard1 = document.getElementById("emptyCard1");
 const emptyCard2 = document.getElementById("emptyCard2");
 const emptyCard3 = document.getElementById("emptyCard3");
 const emptyCard4 = document.getElementById("emptyCard4");
-let cardList = ["Pokemon Cards/bulbasaur.webp", "Pokemon Cards/charizard.png", "Pokemon Cards/charmander.png", "Pokemon Cards/eevee.png", "Pokemon Cards/pikachu VMAX.png", "Pokemon Cards/pikachu.png", "Pokemon Cards/snorlax.png", "Pokemon Cards/squirtle.png"];
-let newCardList = []
-let randomImage = cardList[Math.floor(Math.random() * cardList.length)];
-console.log(randomImage)
+let cardList = [
+    "Pokemon Cards/bulbasaur.webp",
+    "Pokemon Cards/charizard.png",
+    "Pokemon Cards/charmander.png",
+    "Pokemon Cards/eevee.png",
+    "Pokemon Cards/pikachu VMAX.png",
+    "Pokemon Cards/pikachu.png",
+    "Pokemon Cards/snorlax.png",
+    "Pokemon Cards/squirtle.png"
+];
+
 
 // On click function
 generateBtn.onclick = function generate(){
+    let randomImage = cardList[Math.floor(Math.random() * cardList.length)];
+    console.log(randomImage)
+    changeImage(randomImage)
+};
 
+function changeImage(randomImage){
+    document.getElementById("emptyCard1").src = randomImage;
 };
 
